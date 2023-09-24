92f1a6f613c90a3dc80c65186e0c8186d34479d8
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-09-24 16:54:29 -0700

    Create getWeatherData function to get weather info

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import "./styles.css";
 
-const url = 'https://api.weatherapi.com/v1/current.json?key=f565f05ebecc44d0b0d25131232309&q=garden-grove'
+let url = 'https://api.weatherapi.com/v1/current.json?key=f565f05ebecc44d0b0d25131232309&q=garden-grove'
 
 async function getWeatherData() {
-  const weatherData = await fetch(url, {mode: 'cors'});
-  weatherData.json().then(function(response) {
-    console.log(response);
-  });
+  const weatherDataResponse = await fetch(url, {mode: 'cors'});
+  const weatherData = await weatherDataResponse.json();
+  return weatherData;
 }
 
-getWeatherData();
+getWeatherData()
+  .then(weatherData => {
+    console.log(weatherData);
+  })
+  .catch(error => {
+    console.log(error);
+  })
