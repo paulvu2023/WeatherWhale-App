fb37cc822d3fe313c2b83c1a3b40acb4cc2d851f
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-09-24 15:40:42 -0700

    Add background image to body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css";
 
-console.log("test")
+const url = 'https://api.weatherapi.com/v1/current.json?key=f565f05ebecc44d0b0d25131232309&q=garden-grove'
+
+async function getWeatherData() {
+  const weatherData = await fetch(url, {mode: 'cors'});
+  weatherData.json().then(function(response) {
+    console.log(response);
+  });
+}
+
+getWeatherData();
