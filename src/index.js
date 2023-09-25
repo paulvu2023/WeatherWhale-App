6adc739b6c324139bc96e406d9c2f9279828a4fb
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-09-24 20:36:09 -0700

    Create current location variable for concision

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 
-let url = 'https://api.weatherapi.com/v1/current.json?key=f565f05ebecc44d0b0d25131232309&q=gardengrove';
+let currentLocation = 'tampa';
+let currentURL = `https://api.weatherapi.com/v1/current.json?key=f565f05ebecc44d0b0d25131232309&q=${currentLocation}`;
 
 async function getWeatherData() {
   try {
-    const weatherDataResponse = await fetch(url, { mode: 'cors' });
+    const weatherDataResponse = await fetch(currentURL, { mode: 'cors' });
     if (!weatherDataResponse.ok) {
       throw new Error(`${weatherDataResponse.status}`);
     }
@@ -23,6 +24,8 @@ function processWeatherData(weatherData) {
     city: weatherData.location.name,
     country: weatherData.location.country,
     localTime: weatherData.location.localtime,
+    tempC: weatherData.current.temp_c,
+    tempF: weatherData.current.temp_f,
     condition: weatherData.current.condition.text,
     humidity: weatherData.current.humidity,
     windMPH: weatherData.current.gust_mph,
