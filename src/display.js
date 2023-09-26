e0d0ac7b74ccb7ff9a246440dc6a2a10c01d996c
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-09-26 12:43:33 -0700

    Structure and style weather information

--- src/display.js
+++ src/display.js
@@ -2,25 +2,31 @@ function displayCurrentWeatherData(processedCurrentData, isFahrenheitActive) {
   const city = document.querySelector('.city');
   const country = document.querySelector('.country');
   const localTime = document.querySelector('.localTime');
-  const humidity = document.querySelector('.humidity');
-  const wind = document.querySelector('.wind');
+  const humidity = document.querySelector('.humidity span');
+  const wind = document.querySelector('.wind span');
   const condition = document.querySelector('.condition');
-  const conditionIcon = document.querySelector('.condition span');
+  const conditionIcon = document.querySelector('.condition-icon-div');
   const temperature = document.querySelector('.temperature');
+
   city.textContent = `${processedCurrentData.city},`;
   country.textContent = processedCurrentData.country;
   localTime.textContent = processedCurrentData.localTime;
   condition.textContent = processedCurrentData.condition;
+  humidity.textContent = `${processedCurrentData.humidity} %`;
 
   if (isFahrenheitActive) {
     temperature.textContent = processedCurrentData.tempF;
-    wind.textContent = processedCurrentData.windMPH;
+    wind.textContent = `${processedCurrentData.windMPH} mph`;
   } else {
     temperature.textContent = processedCurrentData.tempC;
     wind.textContent = processedCurrentData.windKPH;
   }
 
-  //add styling and spans
+  if (!getConditionIcon()) {
+    conditionIcon.innerHTML = `<img class='condition-icon conditionIconColor' src='${processedCurrentData.conditionIcon}'>`;
+  } else {
+    conditionIcon.innerHTML = getConditionIcon();
+  }
 }
 
 export { displayCurrentWeatherData };
