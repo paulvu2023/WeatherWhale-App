b74f3ba3192d4f10b1b3066da55cf7b7c6b9d806
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-09-29 15:50:03 -0700

    Add containers and elements for 24 hours

--- src/index.js
+++ src/index.js
@@ -46,6 +46,51 @@ dailyAndHourlyButtons.forEach((button) => {
   });
 });
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+document.querySelectorAll('.dot').forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    currentSlide(index + 1);
+  });
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  plusSlides(-1);
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  plusSlides(1);
+});
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'flex';
+  dots[slideIndex - 1].className += ' active';
+}
+
 handleCurrentWeatherData();
 handleForecastWeatherData();
 
